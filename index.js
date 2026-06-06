03da3b3c4d7d644bd421949c13f5216a862dc779
miraikairos <kumarhritik631@gmail.com>
2026-06-06 20:43:26 +0530

    env token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "7646314468:AAG9cFzjxrVYBtsjwlUxJS1LZzHKgX1bBr4";
+const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, {
   polling: true,
